28a5863e22020dad1a80ae509088c4eea0485bdf
Roberto Mejía <robertomejiacollazos@gmail.com>
2021-09-07 20:53:32 -0500

    === PÁGINA DE ACERCA  ===

--- js/main.js
+++ js/main.js
@@ -70,6 +70,34 @@
   
 /* ================ NAVEGACIÓN MENÚ FIN ================ */
   
+/* ================ SECCIÓN DE ACERCA ================ */
+
+(() => {
+    const aboutSection = document.querySelector(".about-section"),
+        tabsContainer = document.querySelector(".about-tabs");
+
+    tabsContainer.addEventListener("click", (event) => {
+        /* if event.target contains 'tab-item' class and not contains 'active' class */
+        if (event.target.classList.contains("tab-item") &&
+            !event.target.classList.contains("active")) {
+            const target = event.target.getAttribute("data-target");
+            // deactivate existing active 'tab-item'
+            tabsContainer.querySelector(".active").classList.remove("outer-shadow", "active");
+            // activate new 'tab-item'
+            event.target.classList.add("active", "outer-shadow");
+            // deactivate existing active 'tab-content'
+            aboutSection.querySelector(".tab-content.active").classList.remove("active");
+            // activate new 'tab-content'
+            aboutSection.querySelector(target).classList.add("active");
+        }
+    })
+})();
+
+function bodyScrollingToggle() {
+    document.body.classList.toggle("hidden-scrolling");
+}
+
+/* ================ SECCIÓN DE ACERCA - FIN================ */
 /* ================ Ocultar todas las secciones excepto las activas ================ */
   
   (() =>{
