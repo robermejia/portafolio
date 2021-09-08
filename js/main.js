10bb054ed7b5e8b41130f086f0d9603466ad37cb
Roberto Mejía <robertomejiacollazos@gmail.com>
2021-09-08 15:11:58 -0500

    === PÁGINA DE TESTIMONIOS ===

--- js/main.js
+++ js/main.js
@@ -251,7 +251,60 @@ function popupDetailsToggle(){
 })();
 
  /* ================ SECCIÓN DE PORTAFOLIO (FILTRO Y VENTANA EMERGENTE) ================ */
- 
+
+ /* ================ SECCIÓN DE TESTIMONIO (SLIDER) ================ */  
+
+(() =>{
+
+  const sliderContainer = document.querySelector(".testi-slider-container"),
+  slides = sliderContainer.querySelectorAll(".testi-item"),
+  slideWidth = sliderContainer.offsetWidth,
+  prevBtn = document.querySelector(".testi-slider-nav .prev"),
+  nextBtn = document.querySelector(".testi-slider-nav .next");
+  activeSlide = sliderContainer.querySelector(".testi-item.active");
+  let slideIndex = Array.from(activeSlide.parentElement.children).indexOf(activeSlide);
+
+  // set width of all slides
+  slides.forEach((slide) =>{
+    slide.style.width = slideWidth + "px";
+  })
+  // set width of sliderContainer
+  sliderContainer.style.width = slideWidth * slides.length + "px"; 
+
+  nextBtn.addEventListener("click", () =>{
+    if(slideIndex === slides.length-1){
+      slideIndex = 0;
+    }
+    else{
+      slideIndex++;
+    }
+    slider();
+   })
+
+   prevBtn.addEventListener("click", () =>{
+     if(slideIndex === 0){
+       slideIndex = slides.length-1;
+     }
+     else{
+       slideIndex--;
+     }
+     slider();
+    }) 
+
+    function slider(){
+     // deactivate existing active slides
+     sliderContainer.querySelector(".testi-item.active").classList.remove("active");
+     // activate new slide
+     slides[slideIndex].classList.add("active");
+     sliderContainer.style.marginLeft = - (slideWidth * slideIndex) + "px"; 
+    }
+
+    slider();
+
+})();
+
+ /* ================ SECCIÓN DE TESTIMONIO (SLIDER) - FIN ================ */  
+
 /* ================ OCULTAR TODAS LAS SESSIONES EXCEPTO LAS ACTIVAS ================ */
 
 (() =>{
