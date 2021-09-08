39d07bc0d83c864142ff905a3d3f1ddcf74725c2
Roberto Mejía <robertomejiacollazos@gmail.com>
2021-09-08 15:24:15 -0500

    === PÁGINA DE CONTACTO ===

--- js/main.js
+++ js/main.js
@@ -327,3 +327,42 @@ window.addEventListener("load", () =>{
 });
 
 /* ================ OCULTAR TODAS LAS SESSIONES EXCEPTO LAS ACTIVAS - FIN================ */
+
+/* ================ SECCIÓN DE CONTACTO (VENTANA MODAL FORMULARIO) ================ */
+/* ********** ContactForm ********** */
+
+
+const $form = document.querySelector(".contact-form"),
+$loader = document.querySelector(".contact-form-loader"),
+$response = document.querySelector(".contact-form-response");
+
+$form.addEventListener("submit", (e) => {
+e.preventDefault();
+$loader.classList.remove("none");
+fetch("https://formsubmit.co/ajax/appyouversion@gmail.com", {
+  method: "POST",
+  body: new FormData(e.target),
+})
+  .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+  .then((json) => {
+    console.log(json);
+    location.hash = "#gracias";
+    $form.reset();
+  })
+  .catch((err) => {
+    console.log(err);
+    let message =
+      err.statusText || "Ocurrió un error al enviar, intenta nuevamente";
+    $response.querySelector(
+      "h3"
+    ).innerHTML = `Error ${err.status}: ${message}`;
+  })
+  .finally(() => {
+    $loader.classList.add("none");
+    setTimeout(() => {
+      location.hash = "#close";
+    }, 3000);
+  });
+});
+
+/* ================ SECCIÓN DE CONTACTO (VENTANA MODAL FORMULARIO) - FIN ================ */
