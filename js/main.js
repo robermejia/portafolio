db5c94293212a69623a37fc34f26581561261319
Roberto Mejía <robertomejiacollazos@gmail.com>
2022-09-19 17:23:12 -0500

    Primera modificación 2022

--- js/main.js
+++ js/main.js
@@ -254,54 +254,54 @@ function popupDetailsToggle(){
 
  /* ================ SECCIÓN DE TESTIMONIO (SLIDER) ================ */  
 
-(() =>{
+// (() =>{
 
-  const sliderContainer = document.querySelector(".testi-slider-container"),
-  slides = sliderContainer.querySelectorAll(".testi-item"),
-  slideWidth = sliderContainer.offsetWidth,
-  prevBtn = document.querySelector(".testi-slider-nav .prev"),
-  nextBtn = document.querySelector(".testi-slider-nav .next");
-  activeSlide = sliderContainer.querySelector(".testi-item.active");
-  let slideIndex = Array.from(activeSlide.parentElement.children).indexOf(activeSlide);
+//   const sliderContainer = document.querySelector(".testi-slider-container"),
+//   slides = sliderContainer.querySelectorAll(".testi-item"),
+//   slideWidth = sliderContainer.offsetWidth,
+//   prevBtn = document.querySelector(".testi-slider-nav .prev"),
+//   nextBtn = document.querySelector(".testi-slider-nav .next");
+//   activeSlide = sliderContainer.querySelector(".testi-item.active");
+//   let slideIndex = Array.from(activeSlide.parentElement.children).indexOf(activeSlide);
 
-  // set width of all slides
-  slides.forEach((slide) =>{
-    slide.style.width = slideWidth + "px";
-  })
-  // set width of sliderContainer
-  sliderContainer.style.width = slideWidth * slides.length + "px"; 
+//   // set width of all slides
+//   slides.forEach((slide) =>{
+//     slide.style.width = slideWidth + "px";
+//   })
+//   // set width of sliderContainer
+//   sliderContainer.style.width = slideWidth * slides.length + "px"; 
 
-  nextBtn.addEventListener("click", () =>{
-    if(slideIndex === slides.length-1){
-      slideIndex = 0;
-    }
-    else{
-      slideIndex++;
-    }
-    slider();
-   })
+//   nextBtn.addEventListener("click", () =>{
+//     if(slideIndex === slides.length-1){
+//       slideIndex = 0;
+//     }
+//     else{
+//       slideIndex++;
+//     }
+//     slider();
+//    })
 
-   prevBtn.addEventListener("click", () =>{
-     if(slideIndex === 0){
-       slideIndex = slides.length-1;
-     }
-     else{
-       slideIndex--;
-     }
-     slider();
-    }) 
+//    prevBtn.addEventListener("click", () =>{
+//      if(slideIndex === 0){
+//        slideIndex = slides.length-1;
+//      }
+//      else{
+//        slideIndex--;
+//      }
+//      slider();
+//     }) 
 
-    function slider(){
-     // deactivate existing active slides
-     sliderContainer.querySelector(".testi-item.active").classList.remove("active");
-     // activate new slide
-     slides[slideIndex].classList.add("active");
-     sliderContainer.style.marginLeft = - (slideWidth * slideIndex) + "px"; 
-    }
+//     function slider(){
+//      // deactivate existing active slides
+//      sliderContainer.querySelector(".testi-item.active").classList.remove("active");
+//      // activate new slide
+//      slides[slideIndex].classList.add("active");
+//      sliderContainer.style.marginLeft = - (slideWidth * slideIndex) + "px"; 
+//     }
 
-    slider();
+//     slider();
 
-})();
+// })();
 
  /* ================ SECCIÓN DE TESTIMONIO (SLIDER) - FIN ================ */  
 
